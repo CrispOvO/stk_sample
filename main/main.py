5c9a7e67cbef885688d0ac9329b65716b412697b
CRISPOVO\Crisp <1154230660@qq.com>
2023-09-27 14:16:50 +0800

    test

--- main/main.py
+++ main/main.py
@@ -1,7 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.curdir))
+'''
+
+'''
 from obj.SatelliteUtils import Satellite
 from init.ScenarioUtils import Scenario
 
 if __name__ == '__main__':
     readScenario = True
-    sc = Scenario(readScenario)
-    print(sc)
+    # sc = Scenario(readScenario)
+    # print(sc)
